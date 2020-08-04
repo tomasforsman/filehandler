6a494e5a9080f7226ba119e98dd1ac935549c223
Tomas Forsman <tomas@forsman.dev>
2020-08-04 08:55:55 +0200

    Add temporary interface for positive filewatcher response.

--- FileWatcher.Service/Contracts/YesItIs.cs
+++ FileWatcher.Service/Contracts/YesItIs.cs
@@ -1,6 +1,13 @@
+using System;
+using System.Dynamic;
+
 namespace FileWatcher.Service.Contracts
 {
     public interface YesItIs
     {
+        Guid FileGuid { get; }
+        string FileName { get; }
+        string FilePath { get; }
+        DateTime FileSeen { get; }
     }
 }
