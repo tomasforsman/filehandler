e74dbf6f40ec45ed727745932b6d207759d8a4f7
Tomas Forsman <tomas@forsman.dev>
2020-08-04 08:55:02 +0200

    Add simple circuit breaker to saga state machine.

---
+++ FileHandler.Components/StateMachines/FileHandlerStateMachineDefinition.cs
@@ -0,0 +1,21 @@
+﻿using GreenPipes;
+using MassTransit;
+using MassTransit.Definition;
+
+namespace FileHandler.Components.StateMachines
+{
+    public class FileHandlerStateMachineDefinition :
+        SagaDefinition<FileInfoState>
+    {
+        public FileHandlerStateMachineDefinition()
+        {
+            ConcurrentMessageLimit = 4;
+        }
+        protected override void ConfigureSaga(IReceiveEndpointConfigurator endpointConfigurator, ISagaConfigurator<FileInfoState> sagaConfigurator)
+        {
+            // base.ConfigureSaga(endpointConfigurator, sagaConfigurator);
+            endpointConfigurator.UseMessageRetry(r => r.Intervals(500, 5000, 10000));
+            endpointConfigurator.UseInMemoryOutbox();
+        }
+    }
+}
--- FileHandler.Service/Program.cs
+++ FileHandler.Service/Program.cs
@@ -65,7 +65,7 @@ namespace FileHandler.Service
                     {
                         cfg.AddConsumersFromNamespaceContaining<SubmitFileInfoConsumer>();
 
-                        cfg.AddSagaStateMachine<FileHandlerStateMachine, FileInfoState>()
+                        cfg.AddSagaStateMachine<FileHandlerStateMachine, FileInfoState>(typeof(FileHandlerStateMachineDefinition))
                             .RedisRepository();
 
                         cfg.AddBus(ConfigureBus);
