b64cd876b5147bce564452f359b8d773c7043df8
Tomas Forsman <tomas@forsman.dev>
2020-08-18 12:23:32 +0200

    Add projects for service: FileSender

---
+++ FileSender.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSender.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
