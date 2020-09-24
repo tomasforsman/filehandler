0c0540dbdf4e45f31dbc1523c3a6eac216bb9998
Tomas Forsman <tomas@forsman.dev>
2020-09-24 08:51:02 +0200

    Sandbox
    Sandbox console with sender, publisher and listener

---
+++ Sandbox/ConsoleBasic/ConsoleBasicSender/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FileReader.Contracts;
+using MassTransit;
+
+namespace ConsoleBasicSender
+{
+    public static class Program
+    {
+        
+
+        public static async Task Main()
+        {
+            
+            var busControl = Bus.Factory.CreateUsingRabbitMq();
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+            try
+            {
+                do
+                {
+                    var endpoint = await busControl.GetSendEndpoint(new Uri("queue:file-reader"));
+
+                    await endpoint.Send<ReadFile>(new
+                    {
+                        FileId = InVar.Id,
+                        FileName = "filnamn.xml",
+                        FolderName = "svefakt"
+                    },source.Token);
+                    break;
+                }
+                while (true);
+            }
+            finally
+            {
+                await busControl.StopAsync(source.Token);
+            }
+        }
+    }
+}
