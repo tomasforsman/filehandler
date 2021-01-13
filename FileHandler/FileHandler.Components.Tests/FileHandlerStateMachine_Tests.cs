eb0f0555931c164fb8959fa29af80bc1fcf9ada6
tomasforsman <tomas@tomasforsman.com>
2021-01-13 15:35:52 +0100

    Add more unit tests
    Unit tests added to check status changes for FileReader and PriWebCommincator responses.

--- FileHandler/FileHandler.Components.Tests/FileHandlerStateMachine_Tests.cs
+++ FileHandler/FileHandler.Components.Tests/FileHandlerStateMachine_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Automatonymous.Graphing;
@@ -36,10 +37,11 @@ namespace FileHandler.Components.Tests
 
         await harness.Bus.Publish<FileInfoSubmitted>(new
         {
-          FileId = fileId,
-          InVar.Timestamp,
-          FileName = "filename.file",
-          LocalFolder = "c:/folder/"
+            FileId = fileId,
+            InVar.Timestamp,
+            FileName = "filename.file",
+            LocalFolder = "c:/folder/",
+            OriginFolder = "c:/folder/"
         });
         var instanceID = await saga.Exists(fileId, x => x.File_Is_Submitted);
         var instance = saga.Sagas.Contains(instanceID.Value);
@@ -69,13 +71,15 @@ namespace FileHandler.Components.Tests
 
         await harness.Bus.Publish<FileInfoSubmitted>(new
         {
-          FileId = fileId,
-          InVar.Timestamp,
-          FileName = "filename.file",
-          LocalFolder = "c:/folder/"
+            FileId = fileId,
+            InVar.Timestamp,
+            FileName = "filename.file",
+            LocalFolder = "c:/folder/",
+            OriginFolder = "c:/folder/"
         });
-        var instanceID = await saga.Exists(fileId, x => x.File_Is_Submitted);
-        var instance = saga.Sagas.Contains(instanceID.Value);
+        var instanceId = await saga.Exists(fileId, x => x.File_Is_Submitted);
+        Debug.Assert(instanceId != null, nameof(instanceId) + " != null");
+        var instance = saga.Sagas.Contains(instanceId.Value);
 
         var requestClient = await harness.ConnectRequestClient<CheckFileInfo>();
 
@@ -100,7 +104,147 @@ namespace FileHandler.Components.Tests
 
       var dots = generator.CreateDotFile();
 
-      Console.WriteLine(dots);
+      output.WriteLine(dots);
     }
+  }
+
+  public class FileReader_responds
+  {
+    private readonly ITestOutputHelper output;
+
+    public FileReader_responds(ITestOutputHelper output)
+    {
+      this.output = output;
+    }
+    
+    [Fact]
+    public async Task Status_should_be_file_has_been_read()
+    {
+      // Arrange (Given)
+      var harness = new InMemoryTestHarness();
+      var fileHandlerStateMachine = new FileHandlerStateMachine();
+      var saga = harness.StateMachineSaga<FileHandlerState, FileHandlerStateMachine>(fileHandlerStateMachine);
+
+      await harness.Start();
+      try
+      {
+        var fileId = NewId.NextGuid();
+        
+        await harness.Bus.Publish<FileInfoSubmitted>(new
+        {
+          FileId = fileId,
+          InVar.Timestamp,
+          FileName = "filename.file",
+          LocalFolder = "c:/folder/",
+          OriginFolder = "c:/folder/"
+        });
+        
+        //await Task.Delay(2000);
+
+        await saga.Exists(fileId, x => x.File_Is_Submitted);
+        await harness.Bus.Publish<FileRead>(new
+        {
+            FileId = fileId,
+            BuyerId = default(string),
+            SellerId = default(string),
+            InvoiceId = default(string)
+        });
+        
+        var instanceId = await saga.Exists(fileId, x => x.File_Has_Been_Read);
+        output.WriteLine(instanceId.ToString());
+        
+
+        Debug.Assert(instanceId != null, nameof(instanceId) + " != null");
+        var instance = saga.Sagas.Contains(instanceId.Value);
+
+        var requestClient = await harness.ConnectRequestClient<CheckFileInfo>();
+
+        var response = await requestClient.GetResponse<FileStatus>(new {FileId = fileId});
+
+        Assert.Equal(response.Message.State, fileHandlerStateMachine.File_Has_Been_Read.Name);
+      }
+      finally
+      {
+        await harness.Stop();
+      }
+    }
+    
+  }
+  
+  public class PriWebCommunicator_responds
+  {
+    private readonly ITestOutputHelper output;
+
+    public PriWebCommunicator_responds(ITestOutputHelper output)
+    {
+      this.output = output;
+    }
+    
+    [Fact]
+    public async Task Status_should_be_FTP_Settings_Has_Been_Retrieved()
+    {
+      // Arrange (Given)
+      var harness = new InMemoryTestHarness();
+      var fileHandlerStateMachine = new FileHandlerStateMachine();
+      var saga = harness.StateMachineSaga<FileHandlerState, FileHandlerStateMachine>(fileHandlerStateMachine);
+
+      await harness.Start();
+      try
+      {
+        var fileId = NewId.NextGuid();
+        
+        await harness.Bus.Publish<FileInfoSubmitted>(new
+        {
+          FileId = fileId,
+          InVar.Timestamp,
+          FileName = "filename.file",
+          LocalFolder = "c:/folder/",
+          OriginFolder = "c:/folder/"
+        });
+        
+        //await Task.Delay(2000);
+
+        await saga.Exists(fileId, x => x.File_Is_Submitted);
+        await harness.Bus.Publish<FileRead>(new
+        {
+          FileId = fileId,
+          BuyerId = default(string),
+          SellerId = default(string),
+          InvoiceId = default(string)
+        });
+        
+        await saga.Exists(fileId, x => x.File_Has_Been_Read);
+        await harness.Bus.Publish<CommunicationSettingsFound>(new
+        {
+
+            FileId = fileId,
+            Protocol = default(string),
+            HostName = default(string),
+            RemoteFolder = default(string),
+            Password = default(string),
+            UserName = default(string),
+            Port = default(string),
+            SshHostKeyFingerprint = default(string)
+        });
+        
+        var instanceId = await saga.Exists(fileId, x => x.Ftp_Settings_Has_Been_Retrieved);
+        output.WriteLine(instanceId.ToString());
+        
+
+        Debug.Assert(instanceId != null, nameof(instanceId) + " != null");
+        var instance = saga.Sagas.Contains(instanceId.Value);
+
+        var requestClient = await harness.ConnectRequestClient<CheckFileInfo>();
+
+        var response = await requestClient.GetResponse<FileStatus>(new {FileId = fileId});
+
+        Assert.Equal(response.Message.State, fileHandlerStateMachine.Ftp_Settings_Has_Been_Retrieved.Name);
+      }
+      finally
+      {
+        await harness.Stop();
+      }
+    }
+    
   }
 }
